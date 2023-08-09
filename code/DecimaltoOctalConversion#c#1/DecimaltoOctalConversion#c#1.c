a23a3c53120ea54ab9db0b7f55906e2fe2409493
Rahul Kumar Gupta <121859773+Rahulgupta79@users.noreply.github.com>
2023-08-09 09:30:59 +0530

    All types of code C

---
+++ code/DecimaltoOctalConversion#c#1/DecimaltoOctalConversion#c#1.c
@@ -0,0 +1,18 @@
+ #include<stdio.h>
+int main()
+{
+	long int decimalNumber,quotient;
+	int octalNumber[100],i=1,j;
+	printf("Enter any decimal number: ");
+	scanf("%ld",&decimalNumber);
+	quotient = decimalNumber;
+	while(quotient!=0)
+	{
+		octalNumber[i++]=quotient%8;
+		quotient=quotient/8;
+	}
+	printf("Equivalent octal value of decimal number %d: ",decimalNumber);
+	for(j=i-1 ;j>0; j--)
+		printf("%d",octalNumber[j]);
+	return 0;
+}
